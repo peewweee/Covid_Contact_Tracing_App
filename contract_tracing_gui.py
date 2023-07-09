29755c173414fc6a60a5b0d5a751178142d28c4c
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-09 18:58:52 +0800

    start button for first part interface

---
+++ contract_tracing_gui.py
@@ -0,0 +1,19 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Trace Me")
+window.geometry("840x440")
+window.configure(bg="#934AB3")
+
+frame = tkinter.Frame(bg="#934AB3")
+
+label_name = tkinter.Label(frame, text="Trace Me", bg="#934AB3", fg="#F7E7FB", font=("Arial", 18))
+start_button = tkinter.Button(frame, text="Start", bg="#F5AE52", fg="#F76301", font=("Arial",16))
+label_message = tkinter.Label(frame, text="Tara na sa ligtas na gala!", bg="#934AB3", fg="#E0C2F2", font=("Arial", 13))
+
+label_name.grid(row=0, column=0, pady=30)
+start_button.grid(row=1, column=0, pady=10)
+label_message.grid(row=2, column=0)
+
+frame.pack()
+window.mainloop()
