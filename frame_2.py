bb57111cf65bfc500e32064dffc00685250c4ea3
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-24 22:25:55 +0800

    fixed the layout (added LabelFrame)

--- frame_2.py
+++ frame_2.py
@@ -3,34 +3,42 @@ import tkinter.messagebox as messagebox
 
 # frame 2 widgets
 def create_frame_2(parent_frame):
-    frame_2 = tkinter.Frame(parent_frame, bg="#3F1651", width=parent_frame.winfo_width(), height=parent_frame.winfo_height())
+    frame_2 = tkinter.Frame(parent_frame, bg="#3F1651")
+    frame_2.pack(fill=tkinter.BOTH, expand=True)
+
+    # Personal Information Label
+    personal_info = tkinter.LabelFrame(frame_2, text="Part I. Personal Info", bg="#3F1651", fg="#F2C75B", font=("Arial", 12, "bold"))
+    personal_info.pack(pady=20, padx=20, fill=tkinter.BOTH, expand=True)
 
     # Full Name
-    full_name_2 = tkinter.Label(frame_2, text="Full Name:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
-    full_name_2.grid(row=0, column=0, pady=5)
-    entry_name = tkinter.Entry(frame_2, bg="white", fg="black", font=("Arial", 12))
-    entry_name.grid(row=0, column=1, pady=5)
+    full_name_2 = tkinter.Label(personal_info, text="Full Name:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
+    full_name_2.grid(row=1, column=0, padx=5, pady=5)
+    entry_name = tkinter.Entry(personal_info, bg="white", fg="black", font=("Arial", 12))
+    entry_name.grid(row=1, column=1, pady=5)
 
     # Age
-    age_label = tkinter.Label(frame_2, text="Age:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
-    age_label.grid(row=1, column=0, pady=5)
-    entry_age = tkinter.Entry(frame_2, bg="white", fg="black", font=("Arial", 12))
-    entry_age.grid(row=1, column=1, pady=5)
+    age_label = tkinter.Label(personal_info, text="Age:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
+    age_label.grid(row=2, column=0, padx=5, pady=5)
+    entry_age = tkinter.Entry(personal_info, bg="white", fg="black", font=("Arial", 12))
+    entry_age.grid(row=2, column=1, padx=5, pady=5)  # Reduce padx to 5
 
     # Phone Number
-    phone_num = tkinter.Label(frame_2, text="Phone Number:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
-    phone_num.grid(row=2, column=0, pady=5)
-    num_entry = tkinter.Entry(frame_2, bg="white", fg="black", font=("Arial", 12))
-    num_entry.grid(row=2, column=1, pady=5)
+    phone_num = tkinter.Label(personal_info, text="Phone Number:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
+    phone_num.grid(row=1, column=2, padx=5, pady=5)
+    num_entry = tkinter.Entry(personal_info, bg="white", fg="black", font=("Arial", 12))
+    num_entry.grid(row=1, column=3, pady=5)
+
+    # Vaccine and Test
+    vaccine_test = tkinter.LabelFrame(frame_2, text="Part II. COVID-19 Vaccination and Test", bg="#3F1651", fg="#F2C75B", font=("Arial", 12, "bold"))
+    vaccine_test.pack(pady=20, padx=20, fill=tkinter.BOTH, expand=True)
 
     # Vaccine
-    label_vaccine = tkinter.Label(frame_2, text="COVID-19 Vaccination:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
-    label_vaccine.grid(row=3, column=0, pady=10)
+    label_vaccine = tkinter.Label(vaccine_test, text="COVID-19 Vaccination:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
+    label_vaccine.grid(row=4, column=0, pady=10)
     var_vaccine = tkinter.StringVar()
     vaccine_options = ["None", "First Dose", "Second Dose", "First Booster Shot", "Second Booster Shot"]
-    vaccine_dropdown = tkinter.OptionMenu(frame_2, var_vaccine, *vaccine_options)
-    vaccine_dropdown.grid(row=3, column=1, pady=10)
-
+    vaccine_dropdown = tkinter.OptionMenu(vaccine_test, var_vaccine, *vaccine_options)
+    vaccine_dropdown.grid(row=4, column=1, pady=10)
 
     # save information
     def save_info():
@@ -53,8 +61,7 @@ def create_frame_2(parent_frame):
             file.write(f"Phone Number: {save_num}\n")
             file.write(f"Vaccination: {save_vaccine}\n")
 
-
     save_button = tkinter.Button(frame_2, text="Save", bg="#F5AE52", fg="#F76301", font=("Arial", 12), command=save_info)
-    save_button.grid(row=4, column=1, pady=10)
+    save_button.pack(pady=10)
 
     return frame_2
