079b88ee605dd3133b0dda1a4f0fa0237975dc4f
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-25 03:57:50 +0800

    added labelframe Search Information Entries

--- frame_3.py
+++ frame_3.py
@@ -1,6 +1,10 @@
-import tkinter as tk
+import tkinter
 
 def create_frame_3(parent_frame):
-    frame_3 = tk.Frame(parent_frame, bg="#3F1651")
+    frame_3 = tkinter.Frame(parent_frame, bg="#3F1651")
+
+    # Label header
+    info_data = tkinter.LabelFrame(frame_3, text="Search Information Entries", bg="#3F1651", fg="#F2C75B", font=("Arial", 12, "bold"))
+    info_data.pack(pady=20, padx=20, fill=tkinter.BOTH, expand=True)
 
     return frame_3
