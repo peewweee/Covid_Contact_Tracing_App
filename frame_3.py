212be2694d6c1302242ad4a017ddfc47880b8d55
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-25 04:03:30 +0800

    search bar added

--- frame_3.py
+++ frame_3.py
@@ -7,4 +7,10 @@ def create_frame_3(parent_frame):
     info_data = tkinter.LabelFrame(frame_3, text="Search Information Entries", bg="#3F1651", fg="#F2C75B", font=("Arial", 12, "bold"))
     info_data.pack(pady=20, padx=20, fill=tkinter.BOTH, expand=True)
 
+    # Search
+    search_label = tkinter.Label(info_data, text="Search:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
+    search_label.grid(row=0, column=0, padx=10, pady=10)
+    search_entry = tkinter.Entry(info_data, bg="white", fg="black", font=("Arial", 12), width=30)
+    search_entry.grid(row=0, column=1, pady=10)
+
     return frame_3
