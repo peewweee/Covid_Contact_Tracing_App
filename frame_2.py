14302915afea0e94fafa57e5a1109446933ea0b9
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-25 01:56:19 +0800

    added exposure and symptoms

--- frame_2.py
+++ frame_2.py
@@ -19,19 +19,19 @@ def create_frame_2(parent_frame):
 
     # Age
     age_label = tkinter.Label(personal_info, text="Age:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
-    age_label.grid(row=2, column=0, padx=5, pady=0)
+    age_label.grid(row=2, column=0, padx=5, pady=5)
     entry_age = tkinter.Spinbox(personal_info, from_=0, to=150, font=("Arial", 12), width=29)
-    entry_age.grid(row=2, column=1, pady=0)
+    entry_age.grid(row=2, column=1, pady=5)
 
     # Phone Number
     phone_num = tkinter.Label(personal_info, text="Phone Number:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
     phone_num.grid(row=1, column=2, padx=10, pady=10)
-    num_entry = tkinter.Entry(personal_info, bg="white", fg="black", font=("Arial", 12), width=30)  # Set the width
+    num_entry = tkinter.Entry(personal_info, bg="white", fg="black", font=("Arial", 12), width=30)
     num_entry.grid(row=1, column=3, pady=10)
 
     # Vaccine and Test
     vaccine_test = tkinter.LabelFrame(frame_2, text="Part II. COVID-19 Vaccination and Test", bg="#3F1651", fg="#F2C75B", font=("Arial", 12, "bold"))
-    vaccine_test.pack(pady=10, padx=20, fill=tkinter.BOTH, expand=True)
+    vaccine_test.pack(pady=5, padx=20, fill=tkinter.BOTH, expand=True)
 
     # Vaccine
     label_vaccine = tkinter.Label(vaccine_test, text="COVID-19 Vaccination:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
@@ -40,17 +40,59 @@ def create_frame_2(parent_frame):
     vaccine_options.grid(row=0, column=1, pady=5)
 
     # Test
-    label_test = tkinter.Label(vaccine_test, text="COVID-19 Test:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
+    label_test = tkinter.Label(vaccine_test, text="COVID-19 Test in the last 14 days:", bg="#3F1651", fg="#E6A1D7", font=("Arial", 12))
     label_test.grid(row=0, column=2, padx=10, pady=5)
     test_options = ttk.Combobox(vaccine_test, values=["Not been tested", "Tested (Positive)", "Tested (Negative)", "Tested (Pending Results)"])
     test_options.grid(row=0, column=3, pady=5)
 
+    # Symptoms and Exposure
+    symptoms_expo = tkinter.LabelFrame(frame_2, text="Part III. Symptoms and Exposure", bg="#3F1651", fg="#F2C75B", font=("Arial", 12, "bold"))
+    symptoms_expo.pack(pady=10, padx=20, fill=tkinter.BOTH, expand=True)
+
+    # Exposure
+    label_exposure = tkinter.Label(symptoms_expo, text="Exposure in the last 14 days (Check all that apply):", bg="#3F1651", fg="#E6A1D7", font=("Arial", 11))
+    label_exposure.grid(row=0, column=0, padx=10, pady=5)
+    exposure_options = ["Exposure to cluster of individuals with flu-like illness", "Exposure to confirm case of COVID-19", "Exposure to suspect case for COVID-19", "None"]
+    var_exposure = []
+
+    for idx, option in enumerate(exposure_options):
+        var_exposure.append(tkinter.IntVar())
+        check_button = tkinter.Checkbutton(symptoms_expo, text=option, variable=var_exposure[idx], bg="#3F1651", fg="#E6A1D7", font=("Arial", 10))
+        check_button.grid(row=idx + 1, column=0, padx=10, sticky="w")
+
+    # Symptoms
+    label_symptoms = tkinter.Label(symptoms_expo, text="Symptoms experienced in the past 7 days (Check all that apply):", bg="#3F1651", fg="#E6A1D7", font=("Arial", 11))
+    label_symptoms.grid(row=0, column=1, columnspan=2, padx=10, pady=5, sticky="n")
+    symptoms_options = ["Fever", "Cough", "Colds", "Muscle/body pains", "Sore throat", "Diarrhea", "Headache", "Shortness of breath", "Difficulty of breathing", "Loss of taste", "Loss of smell", "None of the above"]
+    var_symptoms = []
+
+    for idx, option in enumerate(symptoms_options[:6]):
+        var_symptoms.append(tkinter.IntVar())
+        check_button2 = tkinter.Checkbutton(symptoms_expo, text=option, variable=var_symptoms[idx], bg="#3F1651", fg="#E6A1D7", font=("Arial", 10))
+        check_button2.grid(row=idx + 1, column=1, padx=10, sticky="w")
+
+    for idx, option in enumerate(symptoms_options[6:]):
+        var_symptoms.append(tkinter.IntVar())
+        check_button2 = tkinter.Checkbutton(symptoms_expo, text=option, variable=var_symptoms[6 + idx], bg="#3F1651", fg="#E6A1D7", font=("Arial", 10))
+        check_button2.grid(row=idx + 1, column=2, padx=10, sticky="w")
+
     # save information
     def save_info():
         save_name = entry_name.get()
-        save_age = int(entry_age.get())  # Convert the age to an integer
+        save_age = int(entry_age.get())
         save_num = num_entry.get()
         save_vaccine = vaccine_options.get()
+
+        # error window for phone number
+        if not save_num.isdigit():
+            messagebox.showerror("Error", "Please enter a valid phone number (numeric value).")
+            return
+
+        with open("info_contract_tracing.txt", "a") as file:
+            file.write(f"Name: {save_name}\n")
+            file.write(f"Age: {save_age}\n")
+            file.write(f"Phone Number: {save_num}\n")
+            file.write(f"Vaccination: {save_vaccine}\n")
 
         # error window for phone number
         if not save_num.isdigit():
