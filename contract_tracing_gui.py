92a0ca2df3207e5d7fb4abde457832f2a59e17ae
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-07-25 19:11:30 +0800

    fix layout of first frame

--- contract_tracing_gui.py
+++ contract_tracing_gui.py
@@ -18,8 +18,12 @@ bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 frame = tkinter.Frame(bg="#653780")
 
 # frame 1 widgets
-start_button = tkinter.Button(frame, text="Start", bg="#F5AE52", fg="#F76301", font=("Arial", 16), command=show_next_page)
-start_button.grid(row=3, column=2, pady=10)
+
+start_button = tkinter.Button(frame, text="Start", bg="#1B7979", fg="#F2C75B", font=("Arial", 16, "bold"), width=10, command=show_next_page)
+start_button.grid(row=0, column=0, padx=10, pady=45)
+
+search_button = tkinter.Button(frame, text="Search", bg="#E53F71", fg="#3F1651", font=("Arial", 16, "bold"), width=10, command=save_info)
+search_button.grid(row=0, column=1, padx=10, pady=45)
 
 frame.pack()
 
